713d4f766f157e8ab7035a69eb9efd761d009848
projectperfect <jprfct@protonmail.com>
2022-08-18 19:27:48 -0500

    project completed.

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ def random_color():
     color = (r, g, b)
     return color
 
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
